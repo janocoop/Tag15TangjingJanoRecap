a3c0e5f6391ae443815fb9dc8f8e711ce8f1da53
janocoop <jano.dreier@coop.ch>
2023-12-13 08:37:07 +0100

    Initial

---
+++ src/main/java/com/example/tag15tangjingjanorecap/Tag15TangjingJanoRecapApplication.java
@@ -0,0 +1,13 @@
+package com.example.tag15tangjingjanorecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tag15TangjingJanoRecapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tag15TangjingJanoRecapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tag15tangjingjanorecap/Tag15TangjingJanoRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tag15tangjingjanorecap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tag15TangjingJanoRecapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
